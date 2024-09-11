9029d25e6fb9c539ac097c614c5d6e9c3ce9cb42
Natthalie - Notebook/IdeaPad <natthaliebohm@hotmail.com>
2024-09-11 19:05:11 -0300

    spring initializr certo

---
+++ HabitUAU_WEB/HabitUAU_WEB/src/main/java/com/habituau/HabitUAU_WEB/HabitUauWebApplication.java
@@ -0,0 +1,13 @@
+package com.habituau.HabitUAU_WEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitUauWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitUauWebApplication.class, args);
+	}
+
+}
---
+++ HabitUAU_WEB/HabitUAU_WEB/src/test/java/com/habituau/HabitUAU_WEB/HabitUauWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habituau.HabitUAU_WEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitUauWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
